01f895ebe004336eef551b0861c7b2252697e12d
Michel <mmunhz@gmail.com>
2025-06-02 15:28:11 -0300

    feat(game): add clickable item box factory

--- include/games/BoxSelection/clickableBoxFactory.hpp
+++ include/games/BoxSelection/clickableBoxFactory.hpp
@@ -1,28 +1,52 @@
 #pragma once
 
 #include "engine/components/Clickable.hpp"
+#include "engine/components/GlobalTransformable.hpp"
+#include "engine/components/RenderRectangle.hpp"
+#include "engine/components/Transformable.hpp"
 #include "engine/core/Types.hpp"
 #include "games/BoxSelection/itemBoxFactory.hpp"
+#include "raylib.h"
+#include <cstdint>
 
-class ClickableBoxFactory
-{
+class ClickableBoxFactory {
 public:
   static Entity creatClickableBox(World &world, int quantity, Vector2 origin,
                                   Vector2 position, Vector2 size,
                                   float rotation, uint32_t priority,
-                                  Color boxColor, Color itemColor)
-  {
+                                  Color boxColor, Color itemColor) {
     Entity box =
         itemBoxFactory::createItemBox(world, quantity, origin, position, size,
                                       rotation, priority, boxColor, itemColor);
-    setupSystem(world, box);
+
+    Entity cover = createCover(world, box, priority);
+    setupSystem(world, cover);
     return box;
   }
 
-  static void setupSystem(World &world, Entity e)
-  {
-    world.attach<Clickable>(
-        e, Clickable([](Entity e) { std::cout << "on click\n"; },
-                     [](Entity e) { std::cout << "on release\n"; }));
+private:
+  static Entity createCover(World &world, Entity parent, uint32_t priority) {
+    Entity cover = world.entityManager.create();
+    world.attach<Transformable>(
+        cover,
+        Transformable({0.5f, 0.5f}, {0.5f, 0.5f}, {1.0f, 1.0f}, 0.0f, parent));
+    world.attach<GlobalTransformable>(cover, {});
+    world.attach<RenderRectangle>(
+        cover,
+        {RenderRectangle(priority + 1, Vector4{0.0f, 0.0f, 0.0f, 0.0f})});
+    return cover;
+  }
+
+  static void setupSystem(World &world, Entity e) {
+    auto &rectRender = world.get<RenderRectangle>(e);
+    world.attach<Clickable>(e, Clickable(
+                                   [&](Entity e) {
+                                     rectRender.color.a = 153;
+                                     std::cout << "on click\n";
+                                   },
+                                   [&](Entity e) {
+                                     rectRender.color.a = 0;
+                                     std::cout << "on release\n";
+                                   }));
   }
 };
